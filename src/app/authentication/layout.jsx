e5ea478388bd9df813eff1757d0fa1d7d6126bd5
Long Zhao <zlong@outlook.com>
2026-04-07 21:47:12 +1000

    feat: add MainLayout component and update AuthenticationLayout with Header

--- src/app/authentication/layout.jsx
+++ src/app/authentication/layout.jsx
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
 import Showcase from "./components/Showcase";
 
 const AuthenticationLayout = ({ children }) => (
-  <div className="flex *:flex-1 p-8 min-h-dvh">
+  <div className="relative flex *:flex-1 min-h-dvh">
+    <Header ghost />
     <div className="px-[125px] my-auto">{children}</div>
     <Showcase />
   </div>
