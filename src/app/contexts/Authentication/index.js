e4ddd4aedf90e7f2f93a310ea52a9d1900560470
Long Zhao <zlong@outlook.com>
2026-04-14 19:54:45 +1000

    test: enhance sign up form tests with authentication context and API integration

--- src/app/contexts/Authentication/index.js
+++ src/app/contexts/Authentication/index.js
@@ -1,2 +1,3 @@
+export { default as AuthenticationContext } from "./AuthenticationContext";
 export { default as AuthenticationProvider } from "./AuthenticationProvider";
 export { default as useAuthentication } from "./useAuthentication";
